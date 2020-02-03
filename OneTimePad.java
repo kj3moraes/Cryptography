dd1a331624c76e73184d2019f6304ef02da39847
Keane Moraes <lordvader3002@gmail.com>
2020-02-03 21:06:52 +0400

    Update OneTimePad.java

--- OneTimePad.java
+++ OneTimePad.java
@@ -1,10 +1,10 @@
 class OneTimePad {
 	public static void main(String[] args) {
 		Scanner txt = new Scanner(System.in), num = new Scanner(System.in);
-      System.out.println("Enter your choice \n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
-      int choice = num.nextInt();
-      String plainText = "", encryptedText= "", key = "";
-      switch(choice) {
+      		System.out.println("Enter your choice \n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
+      		int choice = num.nextInt();
+      		String plainText = "", encryptedText= "", key = "";
+      		switch(choice) {
 			case 1:
 				System.out.println("PLAIN TEXT:");
 				plainText = txt.next();
@@ -28,13 +28,13 @@ class OneTimePad {
 
 			case 88:
 
-         case 120:
-         	System.exit(0);
-            break;
+         		case 120:
+         			System.exit(0);
+            			break;
 
-         default:
-            System.out.println("KINDLY ENTER A NUMBER (1-2) or (X) to EXIT ");
-		}//switch statement
+         		default:
+            			System.out.println("KINDLY ENTER A NUMBER (1-2) or (X) to EXIT ");
+			}//switch statement
 	}//end of void main(String[])
 
 	private static String computeBitwiseXOR(String plainText, String key){
