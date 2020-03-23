a44f561b367a85ac0f2ba09d5f6b6f5027b44f99
Keane Moraes <lordvader3002@gmail.com>
2020-03-23 12:15:10 +0400

    --CLEANING--

--- Checkerboard.java
+++ Checkerboard.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Scanner;
-class CheckerboardCipher {
+class Checkerboard {
     public static void main(String[] args) {
         Scanner txt = new Scanner(System.in), num = new Scanner(System.in);
         System.out.println("Enter your choice \n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
