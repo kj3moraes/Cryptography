07c0ae67c231a1723c09a705fbc552d5db25efa8
Keane Moraes <xclbr@localhost.localdomain>
2020-06-17 23:49:31 -0400

    --CLEANING--

--- AES/SPNetwork.java
+++ AES/SPNetwork.java
@@ -3,6 +3,40 @@ import java.util.Scanner;
 class SPNetwork {
     public static void main(String[] args){
         Scanner num = new Scanner(System.in), txt = new Scanner(System.in);
-        
+        System.out.println("\nEnter your choice \n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
+        final char choice = num.next().toUpperCase().charAt(0);
+        String plainText, encryptedText, seed;
+        switch (choice) {
+            // ENCODING
+            case '1':
+                System.out.print("\t PLAIN TEXT : ");
+                plainText = txt.nextLine().trim();
+                System.out.print("\t KEY : ");
+                seed = txt.nextLine().toUpperCase().trim();
+                System.out.println("\n\t\t INPUTED PLAIN TEXT : " + plainText);
+                System.out.println("\t\t KEY (seed for Secret Key): " + seed);
+                System.out.println("\t\t GENERATED ENCRYPTION : " + encryptedText);
+                break;
+
+            // DECODING
+            case '2':
+                System.out.print("\t ENCRYPTED TEXT : ");
+                encryptedText = txt.nextLine();
+                System.out.print("\t KEY : ");
+                seed = txt.nextLine().toUpperCase().trim();
+                System.out.println("\n\t\t INPUTED ENCRYPTED TEXT : " + encryptedText);
+                System.out.println("\t\t KEY (seed for Secret Key): " + seed);
+                System.out.println("\t\t GENERATED DECRYPTION : " + plainText);
+                break;
+
+            // EXIT
+            case 'X':
+                System.exit(0);
+                break;
+
+            default:
+                System.out.println("KINDLY ENTER A NUMBER (1-2) or (X) to EXIT ");
+        }//switch statement
+        main(new String[]{});
     }//end of void main(String[])
 }//end of class
