9f12ea7decef07dd1d078ddd25a0cbec4fb4ba03
Keane Moraes <xclbr@localhost.localdomain>
2020-06-17 23:52:29 -0400

    UPDATE 6.2 - ADDED DES, AES Essentials

---
+++ DES/FeistelNetwork.java
@@ -0,0 +1,8 @@
+package DES;
+import java.util.Scanner;
+class FeistelNetwork {
+    public static void main(String[] args){
+        Scanner num = new Scanner(System.in), txt = new Scanner(System.in);
+        
+    }//end of void main(String[])
+}//end of class
---
+++ DES/SBox.java
@@ -0,0 +1,9 @@
+package DES;
+package ;
+import java.util.Scanner;
+class SBox {
+    public static void main(String[] args){
+        Scanner num = new Scanner(System.in), txt = new Scanner(System.in);
+        
+    }//end of void main(String[])
+}//end of class
