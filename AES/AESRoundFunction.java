3053f3f9c5baf74ae019de87f4734f9ac4679b0c
Keane Moraes <xclbr@localhost.localdomain>
2020-07-08 23:15:31 -0400

    UPDATE 6.10 - MIXCOLUMNS

--- AES/AESRoundFunction.java
+++ AES/AESRoundFunction.java
@@ -1,52 +1,50 @@
 package AES;
-
 public class AESRoundFunction {
-    //private static final Scanner num = new Scanner(System.in), txt = new Scanner(System.in);
     private int keySize;
-    private String key;
 
     public AESRoundFunction(){
         keySize = 256;
     }//end of public RoundFunction()
 
-    public AESRoundFunction(int keySize) {
+    public AESRoundFunction(int keySize){
         this.keySize = keySize;
-    }// end of public RoundFunction(int)
+    }//end of public RoundFunction(int)
 
     protected void subBytes(int[][] matrix){
         SBox S = new SBox('e');
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[i].length; j++)
+                matrix[i][j] = S.performSubstitution(matrix[i][j]);
     }//end of void subBytes(int[][])
 
-    protected void shiftRows(int[][] matrix) {
+    protected void shiftRows(int[][] matrix){
         int[] tempArray;
         for (int n = 0; n < 4; n++) {
             tempArray = new int[4];
             for (int i = 0; i < 4; i++)
-                tempArray[i] = matrix[n][(i + n) % 4];
-            matrix[n] = tempArray;
-        } // for loop - n
+                tempArray[i] = matrix[n][(i+n)%4];
+            matrix[n]= tempArray;
+        }//for loop - n
     }// end of void shiftRows(int[][])
 
-    protected void mixColumns(int[][] matrix){
-        int[] tempArray = new int[4];
-        int[][] multMatrix = { {2,3,1,1},
-                               {1,2,3,1},
-                               {1,1,2,3},
-                               {3,1,1,2} };
-        int multValue;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; i++) 
-                tempArray[i] += multMatrix[i][j]*matrix[j][i]; 
-            for (int k = 0; k < 4; k++) 
-                matrix[i][k] = tempArray[k];          
-        }//for loop - i
+    protected void mixColumns(){
     }// end of void mixColumns(int[][])
 
     protected void addRoundKey(int[][] matrix) {
-        
+
     }// end of void addRoundKey(int[][])
 
-    private void keyGenerate(String seed){
-        
-    }//end of void keyGenerate(String)
+    private int dotProduct(int a, int b){
+        int result = 0;
+        switch (a){
+            case 2:
+                result = b<<1>=256?(b<<1)^256:b<<1;
+                result = b>=128?result^27:result;
+                break;
+
+            case 3:
+                result = dotProduct(2,b)^b;
+        }//switch statement
+        return result;
+    }//end of int dotProduct(int, int)
 }//end of class
