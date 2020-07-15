6fc66dac614526e1628abe2fcadb29692af773a1
Keane Moraes <lordvader3002@gmail.com>
2020-07-15 20:22:51 +0400

    UPDATE 7.2 - AES INIT KEY + SPEEDUPS

--- AES/AESRoundFunction.java
+++ AES/AESRoundFunction.java
@@ -1,26 +1,15 @@
 package AES;
+
 public class AESRoundFunction {
-    private int keySize;
-    protected String seed;
 
-    public AESRoundFunction(String seed) {
-        this.seed = seed;
-        keySize = 256;
-    }// end of public RoundFunction()
-
-    public AESRoundFunction(int keySize, String seed) {
-        this.seed = seed;
-        this.keySize = keySize;
-    }// end of public RoundFunction(int)
-
-    protected void subBytes(int[][] matrix) {
+    protected static void subBytes(int[][] matrix){
         SBox S = new SBox('e');
         for (int i = 0; i < matrix.length; i++)
             for (int j = 0; j < matrix[i].length; j++)
                 matrix[i][j] = S.performSubstitution(matrix[i][j]);
-    }// end of void subBytes(int[][])
+    }//end of void subBytes(int[][])
 
-    protected void shiftRows(int[][] matrix){
+    protected static void shiftRows(int[][] matrix){
         int[] tempArray;
         for (int n = 0; n < 4; n++) {
             tempArray = new int[4];
@@ -30,35 +19,40 @@ public class AESRoundFunction {
         }//for loop - n
     }// end of void shiftRows(int[][])
 
-    protected void mixColumns(int[][] matrix) {
+    protected static void mixColumns(int[][] matrix){
         for (int i = 0; i < 4; i++) {
-            matrix[0][i] = dotProduct(2, matrix[0][i]) ^ dotProduct(3, matrix[1][i]) ^ matrix[2][i] ^ matrix[3][i];
+            matrix[0][i] = dotProduct(2,matrix[0][i]) ^
+                    dotProduct(3,matrix[1][i]) ^ matrix[2][i] ^ matrix[3][i];
 
-            matrix[1][i] = matrix[0][i] ^ dotProduct(2, matrix[1][i]) ^ dotProduct(3, matrix[2][i]) ^ matrix[3][i];
+            matrix[1][i] = matrix[0][i] ^ dotProduct(2,matrix[1][i]) ^
+                    dotProduct(3,matrix[2][i]) ^ matrix[3][i];
 
-            matrix[2][i] = matrix[0][i] ^ matrix[1][i] ^ dotProduct(2, matrix[2][i]) ^ dotProduct(3, matrix[3][i]);
+            matrix[2][i] = matrix[0][i] ^ matrix[1][i] ^
+                    dotProduct(2,matrix[2][i]) ^ dotProduct(3,matrix[3][i]);
 
-            matrix[3][i] = dotProduct(3, matrix[0][i]) ^ matrix[1][i] ^ matrix[2][i] ^ dotProduct(2, matrix[3][i]);
-        }//for loop - i
+            matrix[3][i] = dotProduct(3,matrix[0][i]) ^
+                    matrix[1][i] ^ matrix[2][i] ^ dotProduct(2,matrix[3][i]);
+
+        }
     }// end of void mixColumns(int[][])
 
-    private int dotProduct(int a, int b) {
+    private static int dotProduct(int a, int b){
         int result = 0;
-        switch (a) {
+        switch (a){
             case 2:
-                result = b << 1 >= 256 ? (b << 1) ^ 256 : b << 1;
-                result = b >= 128 ? result ^ 27 : result;
+                result = b<<1>=256?(b<<1)^256:b<<1;
+                result = b>=128?result^27:result;
                 break;
 
             case 3:
-                result = dotProduct(2, b) ^ b;
-        }// switch statement
+                result = dotProduct(2,b)^b;
+        }//switch statement
         return result;
-    }// end of int dotProduct(int, int)
+    }//end of int dotProduct(int, int)
 
-    protected void addRoundKey(int[][] keyMatrix, int[][] matrix) {
+    protected static void addRoundKey(int[][] keyMatrix, int[][] matrix) {
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
                 matrix[i][j] ^= keyMatrix[i][j];
     }// end of void addRoundKey(int[][])
-}// end of class
+}//end of class
--- AES/Keys.java
+++ AES/Keys.java
@@ -1,4 +1,5 @@
 package AES;
+
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -11,33 +12,36 @@ public class Keys {
     private static int[][] currentKeyMatrix;
     private String initialKeyState, salt;
     protected int algorithm, maxColumnNo;
-    private final int[] RCON = {1,2,4,8,16,32,64,128,27,54};
+    private final int[] RCON = { 1, 2, 4, 8, 16, 32, 64, 128, 27, 54 };
 
     public String getInitialKeyState() {
         return initialKeyState;
-    }//end of String getInitKeyState()
+    }// end of String getInitKeyState()
 
-    public String getSalt() { return salt; }//end of String getSalt()
+    public String getSalt() {
+        return salt;
+    }// end of String getSalt()
 
     public Keys(String seed, int algorithm) {
-        //STEP 1 : GENERATE A PSEUDORANDOM KEY BASED ON THE USER'S INPUT
-        SecretKey key = null; byte[] salt = null;
+        // STEP 1 : GENERATE A PSEUDORANDOM KEY BASED ON THE USER'S INPUT
+        SecretKey key = null;
+        byte[] salt = null;
         try {
             SecureRandom sr = SecureRandom.getInstanceStrong();
             salt = new byte[16];
             sr.nextBytes(salt);
-            KeySpec keyGen = new PBEKeySpec(seed.toCharArray(),salt,65536,algorithm);
+            KeySpec keyGen = new PBEKeySpec(seed.toCharArray(), salt, 65536, algorithm);
             key = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1").generateSecret(keyGen);
-        }//try block
+        } // try block
         catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
             System.out.println("ERROR");
             System.exit(0);
-        }//catch block
+        } // catch block
         initialKeyState = ByteArrayToHexadecimal(key.getEncoded());
         this.salt = ByteArrayToHexadecimal(salt);
 
-        //STEP 2 : PROPERLY INITIALIZE THE ALGORITHM AND KEY MATRIX
-        switch(algorithm){
+        // STEP 2 : PROPERLY INITIALIZE THE ALGORITHM AND KEY MATRIX
+        switch (algorithm) {
             case 128:
                 currentKeyMatrix = new int[4][4];
                 this.algorithm = 128;
@@ -55,135 +59,162 @@ public class Keys {
                 this.algorithm = 256;
                 maxColumnNo = 7;
                 break;
-        }//switch statement
-    }//end of public Keys(String, int)
+        }// switch statement
+    }// end of public Keys(String, int)
 
     public Keys(String seed, String salt, int algorithm) {
         SecretKey key = null;
         try {
-            KeySpec keyGen = new PBEKeySpec(seed.toCharArray(),salt.getBytes(),65536,algorithm);
+            KeySpec keyGen = new PBEKeySpec(seed.toCharArray(), salt.getBytes(), 65536, algorithm);
             key = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1").generateSecret(keyGen);
-        }//try block
+        } // try block
         catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
             System.out.println("ERROR");
             System.exit(0);
-        }//catch block
-    }//end of public Keys(String, String, int)
-
-    protected int[][] generateKeyMatrix(int roundNo){
-        int[][] outputMatrix = new int[4][4];
-        
-        //STEP 1 : TAKE CARE OF THE KEY0 STATE FOR ALL POSSIBLE KEY LENGTHS
-        if(roundNo == 0){
-            initialKeyState += " ";
-            for (int i = 0; i < initialKeyState.length()-1; i+=2) {
-                String extract = initialKeyState.substring(i, i + 2);
-               currentKeyMatrix[i/2 % 4][i/8] = Integer.parseInt(extract, 16);
-            }//for loop - i
-            for (int i = 0; i < 4; i++)
-                System.arraycopy(currentKeyMatrix[i], 0, outputMatrix[i], 0, 4);
-            return outputMatrix;
-        }//if statement - INITIAL ROUND
-
-        //STEP 2: HANDLE THE KEY GENERATION PER ALGORITHM
-        int[] column = new int[4];
-        switch (algorithm){
+        } // catch block
+        initialKeyState = ByteArrayToHexadecimal(key.getEncoded());
+        this.salt = salt;
+        // STEP 2 : PROPERLY INITIALIZE THE ALGORITHM AND KEY MATRIX
+        switch (algorithm) {
             case 128:
-                //STEP 2.1.1 : ASSIGN THE LAST COLUMN TO A TEMPORARY VARIABLE.
-                for (int i = 0; i < 4; i++)
-                    column[i] = currentKeyMatrix[i][3];
-
-                //STEP 2.1.2 : GENERATE THE FIRST COLUMN OF THE NEXT KEY MATRIX
-                column = functionF(column, roundNo);
-                for (int i = 0; i < 4; i++) currentKeyMatrix[i][0] ^= column[i];
-                
-                //STEP 2.1.3 : USE THE FIRST COLUMN TO GENERATE THE NEXT COLUMNS
-                for (int i = 1; i <= 3; i++)
-                    for (int j = 0; j < 4; j++)
-                        currentKeyMatrix[j][i] ^= currentKeyMatrix[j][i-1];
-                    break;
+                currentKeyMatrix = new int[4][4];
+                this.algorithm = 128;
+                maxColumnNo = 3;
+                break;
 
             case 192:
-                switch (roundNo%3){
-                    case 0:
-                        break;
-                    case 1 :
-                        break;
-                    case 2:
-                        break;
-                }//switch statement - ROUND NO.
+                currentKeyMatrix = new int[4][6];
+                this.algorithm = 192;
+                maxColumnNo = 5;
                 break;
 
             case 256:
-                switch (roundNo%2){
+                currentKeyMatrix = new int[4][8];
+                this.algorithm = 256;
+                maxColumnNo = 7;
+                break;
+        }// switch statement
+    }// end of public Keys(String, String, int)
+
+    protected int[][] generateKeyMatrix(int roundNo) {
+        int[][] outputMatrix = new int[4][4];
+
+        // STEP 1 : TAKE CARE OF THE KEY0 STATE FOR ALL POSSIBLE KEY LENGTHS
+        if (roundNo == 0) {
+            initialKeyState += " ";
+            for (int i = 0; i < initialKeyState.length() - 1; i += 2) {
+                String extract = initialKeyState.substring(i, i + 2);
+                currentKeyMatrix[i / 2 % 4][i / 8] = Integer.parseInt(extract, 16);
+            } // for loop - i
+            for (int i = 0; i < 4; i++)
+                System.arraycopy(currentKeyMatrix[i], 0, outputMatrix[i], 0, 4);
+            return outputMatrix;
+        } // if statement - INITIAL ROUND
+
+        // STEP 2: HANDLE THE KEY GENERATION PER ALGORITHM
+        int[] column = new int[4];
+        switch (algorithm) {
+            case 128:
+                // STEP 2.1.1 : ASSIGN THE LAST COLUMN TO A TEMPORARY VARIABLE.
+                for (int i = 0; i < 4; i++)
+                    column[i] = currentKeyMatrix[i][3];
+
+                // STEP 2.1.2 : GENERATE THE FIRST COLUMN OF THE NEXT KEY MATRIX
+                column = functionF(column, roundNo);
+                for (int i = 0; i < 4; i++)
+                    currentKeyMatrix[i][0] ^= column[i];
+
+                // STEP 2.1.3 : USE THE FIRST COLUMN TO GENERATE THE NEXT COLUMNS
+                for (int i = 1; i <= 3; i++)
+                    for (int j = 0; j < 4; j++)
+                        currentKeyMatrix[j][i] ^= currentKeyMatrix[j][i - 1];
+                break;
+
+            case 192:
+                switch (roundNo % 3) {
                     case 0:
-                        //STEP 2.3.1 : ASSIGN THE LAST COLUMN TO A TEMPORARY VARIABLE.
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                }// switch statement - ROUND NO.
+                break;
+
+            case 256:
+                switch (roundNo % 2) {
+                    case 0:
+                        // STEP 2.3.1 : ASSIGN THE LAST COLUMN TO A TEMPORARY VARIABLE.
                         for (int i = 0; i < 4; i++)
                             column[i] = currentKeyMatrix[i][7];
 
-                        //STEP 2.3.2 : GENERATE THE FIRST COLUMN OF THE NEXT KEY MATRIX
+                        // STEP 2.3.2 : GENERATE THE FIRST COLUMN OF THE NEXT KEY MATRIX
                         column = functionF(column, roundNo);
-                        for (int i = 0; i < 4; i++) currentKeyMatrix[i][0] ^= column[i];
+                        for (int i = 0; i < 4; i++)
+                            currentKeyMatrix[i][0] ^= column[i];
 
-                        //STEP 2.3.3 : USE THE FIRST COLUMN TO GENERATE THE NEXT COLUMNS
+                        // STEP 2.3.3 : USE THE FIRST COLUMN TO GENERATE THE NEXT COLUMNS
                         for (int i = 1; i <= 3; i++)
                             for (int j = 0; j < 4; j++)
-                                currentKeyMatrix[j][i] ^= currentKeyMatrix[j][i-1];
+                                currentKeyMatrix[j][i] ^= currentKeyMatrix[j][i - 1];
                         break;
 
                     case 1:
-                        //STEP 2.3.1 : ASSIGN THE 4TH COLUMN TO A TEMPORARY VARIABLE.
+                        // STEP 2.3.1 : ASSIGN THE 4TH COLUMN TO A TEMPORARY VARIABLE.
                         for (int i = 0; i < 4; i++)
                             column[i] = currentKeyMatrix[i][3];
 
-                        //STEP 2.3.2 : GENERATE THE FIRST COLUMN OF THE NEXT KEY MATRIX
-                        if(roundNo!=1) column = functionG(column);
+                        // STEP 2.3.2 : GENERATE THE FIRST COLUMN OF THE NEXT KEY MATRIX
+                        if (roundNo != 1)
+                            column = functionG(column);
                         for (int i = 0; i < 4; i++)
                             currentKeyMatrix[i][4] ^= column[i];
 
-                        //STEP 2.3.3 : USE THE FIRST COLUMN TO GENERATE THE NEXT COLUMNS
+                        // STEP 2.3.3 : USE THE FIRST COLUMN TO GENERATE THE NEXT COLUMNS
                         for (int i = 5; i <= 7; i++)
                             for (int j = 0; j < 4; j++)
-                                currentKeyMatrix[j][i] ^= currentKeyMatrix[j][i-1];
+                                currentKeyMatrix[j][i] ^= currentKeyMatrix[j][i - 1];
                         for (int i = 4; i < 8; i++)
                             System.arraycopy(currentKeyMatrix[i], 0, outputMatrix[i - 4], 0, 4);
                         return outputMatrix;
-                }//switch statement - ROUND NO.
+                }// switch statement - ROUND NO.
                 break;
-        }//switch statement - KEY SIZE
+        }// switch statement - KEY SIZE
 
-        //STEP 3 : USE THE CURRENT KEY MATRIX TO ISOLATE A 4x4 PART TO RETURN
+        // STEP 3 : USE THE CURRENT KEY MATRIX TO ISOLATE A 4x4 PART TO RETURN
         for (int i = 0; i < 4; i++)
             System.arraycopy(currentKeyMatrix[i], 0, outputMatrix[i], 0, 4);
 
         return outputMatrix;
-    }//end of int[][] getKeyMatrix(int)
+    }// end of int[][] getKeyMatrix(int)
 
-    private int[] functionG(int[] column){
-        //STEP 1: : ROTWORD
+    private int[] functionG(int[] column) {
+        // STEP 1: : ROTWORD
         int[] resultantKeyColumn = new int[4];
         resultantKeyColumn[3] = column[0];
-        for (int i = 1; i <4 ; i++) resultantKeyColumn[i-1] = column[i];
+        for (int i = 1; i < 4; i++)
+            resultantKeyColumn[i - 1] = column[i];
 
-        //STEP 2 : SUBBYTES
+        // STEP 2 : SUBBYTES
         SBox S = new SBox('e');
-        for (int i = 0; i < 4 ; i++) resultantKeyColumn[i] = S.performSubstitution(resultantKeyColumn[i]);
-        return resultantKeyColumn;
-    }//end of int[] functionG(int[])
-
-    private int[] functionF(int[] column, int roundNo){
-        int[] resultantKeyColumn = new  int[4];
-        resultantKeyColumn = functionG(resultantKeyColumn);
-        //STEP 3 : RCON AND XOR FOR FIRST COLUMN
         for (int i = 0; i < 4; i++)
-            resultantKeyColumn[i] ^= (resultantKeyColumn[i]^RCON[roundNo-1]);
+            resultantKeyColumn[i] = S.performSubstitution(resultantKeyColumn[i]);
         return resultantKeyColumn;
-    }//end of int[] functionF(int[], int)
+    }// end of int[] functionG(int[])
 
-    private String ByteArrayToHexadecimal(byte[] input){
-        String result ="";
+    private int[] functionF(int[] column, int roundNo) {
+        int[] resultantKeyColumn = new int[4];
+        resultantKeyColumn = functionG(resultantKeyColumn);
+        // STEP 3 : RCON AND XOR FOR FIRST COLUMN
+        for (int i = 0; i < 4; i++)
+            resultantKeyColumn[i] ^= (resultantKeyColumn[i] ^ RCON[roundNo - 1]);
+        return resultantKeyColumn;
+    }// end of int[] functionF(int[], int)
+
+    private String ByteArrayToHexadecimal(byte[] input) {
+        String result = "";
         for (byte b : input)
             result += String.format("%02X", b);
         return result;
-    }//end of String ByteArrayToHexadecimal(byte[])
-}//end of class
+    }// end of String ByteArrayToHexadecimal(byte[])
+}// end of class
