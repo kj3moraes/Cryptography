1389f58bac6fb24b05e41759de2d6fc1827a4540
CookieCrumbs19212 <62071086+CookieCrumbs19212@users.noreply.github.com>
2020-09-01 23:21:38 +0400

    Fixes to mixColumns() and addRoundKey()
    * mixColumns() method had a problem where certain values that were required for computing were being overwritten. This has been taken care of using a temporary array to store each column during computing.
    * addRoundKey() had was giving odd outputs when using `matrix[r][c] ^= keyMatrix[r][c]`. It has since been replaced with:
        `matrix[0][c] ^= keyMatrix[0][c]
         matrix[0][c] ^= keyMatrix[0][c]
         matrix[0][c] ^= keyMatrix[0][c]
         matrix[0][c] ^= keyMatrix[0][c]`
    
    This has resolved the issue.

--- AES/AESRoundFunction.java
+++ AES/AESRoundFunction.java
@@ -4,54 +4,71 @@ public class AESRoundFunction {
 
     protected static void subBytes(int[][] matrix){
         SBox S = new SBox('e');
-        for (int i = 0; i < matrix.length; i++)
-            for (int j = 0; j < matrix[i].length; j++)
-                matrix[i][j] = S.performSubstitution(matrix[i][j]);
+        for (int c = 0; c < matrix.length; c++)
+            for (int j = 0; j < matrix[c].length; j++)
+                matrix[c][j] = S.performSubstitution(matrix[c][j]);
+        displayMatrix(matrix);
     }//end of void subBytes(int[][])
 
     protected static void shiftRows(int[][] matrix){
         int[] tempArray;
         for (int n = 0; n < 4; n++) {
             tempArray = new int[4];
-            for (int i = 0; i < 4; i++)
-                tempArray[i] = matrix[n][(i+n)%4];
-            matrix[n]= tempArray;
+            for (int c = 0; c < 4; c++)
+                tempArray[c] = matrix[n][(c+n)%4];
+            matrix[n]= tempArray;            
         }//for loop - n
+        displayMatrix(matrix);
     }// end of void shiftRows(int[][])
 
     protected static void mixColumns(int[][] matrix){
-        for (int i = 0; i < 4; i++) {
-            matrix[0][i] = dotProduct(2,matrix[0][i]) ^
-                    dotProduct(3,matrix[1][i]) ^ matrix[2][i] ^ matrix[3][i];
+        int[] tmp = new int[4];
+        for (int c = 0; c < 4; c++) {
+            tmp[0] = dotProduct(2, matrix[0][c]) ^ dotProduct(3, matrix[1][c]) ^ matrix[2][c] ^ matrix[3][c];
 
-            matrix[1][i] = matrix[0][i] ^ dotProduct(2,matrix[1][i]) ^
-                    dotProduct(3,matrix[2][i]) ^ matrix[3][i];
+            tmp[1]= matrix[0][c] ^ dotProduct(2, matrix[1][c]) ^ dotProduct(3, matrix[2][c]) ^ matrix[3][c];
 
-            matrix[2][i] = matrix[0][i] ^ matrix[1][i] ^
-                    dotProduct(2,matrix[2][i]) ^ dotProduct(3,matrix[3][i]);
+            tmp[2] = matrix[0][c] ^ matrix[1][c] ^ dotProduct(2, matrix[2][c]) ^ dotProduct(3, matrix[3][c]);
 
-            matrix[3][i] = dotProduct(3,matrix[0][i]) ^
-                    matrix[1][i] ^ matrix[2][i] ^ dotProduct(2,matrix[3][i]);
-        }//for loop - i
+            tmp[3] = dotProduct(3, matrix[0][c]) ^ matrix[1][c] ^ matrix[2][c] ^ dotProduct(2, matrix[3][c]);
+        
+            for(int r = 0 ; r < 4 ; r++)
+                matrix[r][c] = tmp[r];
+        }//for loop - c
+        
+        displayMatrix(matrix);
     }// end of void mixColumns(int[][])
 
-    private static int dotProduct(int a, int b){
+    private static int dotProduct(int mulValue, int number){
         int result = 0;
-        switch (a){
+        switch (mulValue) {
             case 2:
-                result = b<<1>=256?(b<<1)^256:b<<1;
-                result = b>=128?result^27:result;
+                result = number << 1 >= 256 ? (number << 1) ^ 256 : number << 1;
+                result = number >= 128 ? result ^ 27 : result;
                 break;
 
             case 3:
-                result = dotProduct(2,b)^b;
-        }//switch statement
+                result = dotProduct(2, number) ^ number;
+        }//switch statement                        
         return result;
     }//end of int dotProduct(int, int)
 
     protected static void addRoundKey(int[][] keyMatrix, int[][] matrix) {
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                matrix[i][j] ^= keyMatrix[i][j];
+        for (int c = 0; c < 4; c++){
+            matrix[0][c] ^= keyMatrix[0][c];
+            matrix[1][c] ^= keyMatrix[1][c];
+            matrix[2][c] ^= keyMatrix[2][c];
+            matrix[3][c] ^= keyMatrix[3][c];
+        }
+        displayMatrix(matrix);                
     }// end of void addRoundKey(int[][])
+
+    private static void displayMatrix(int[][] matrix){
+        for(int r = 0 ; r < 4 ; r++){
+            for(int c = 0 ; c < 4 ; c++){
+                System.out.print(Integer.toHexString(matrix[r][c]) + ", ");
+            }
+            System.out.println();
+        }        
+    }
 }//end of class
