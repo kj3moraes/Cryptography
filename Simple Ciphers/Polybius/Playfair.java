d443ef9dbe3c68201037239b68055a28276623cb
Keane Moraes <lordvader3002@gmail.com>
2020-11-06 20:09:51 +0400

    UPDATE 10.4 - Polybius Key Addition

--- Simple Ciphers/Polybius/Playfair.java
+++ Simple Ciphers/Polybius/Playfair.java
@@ -1,18 +1,20 @@
 package Polybius;
 import java.util.Scanner;
-class Playfair{
+class Playfair {
     public static void main(String[] args) {
         final Scanner txt = new Scanner(System.in), num = new Scanner(System.in);
         System.out.println("Enter your choice \n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
         final char choice = num.next().toUpperCase().charAt(0);
-        String plainText = "", encryptedText = "";
+        String plainText, encryptedText, key;
         switch (choice) {
             case '1':
                 System.out.print("\t PLAIN TEXT : ");
                 plainText = txt.nextLine();
-                encryptedText = encrypt(plainText);
+                System.out.print("\t KEY (every character of the key must be unique) : ");
+                key = txt.nextLine().toUpperCase();
+                encryptedText = encrypt(plainText, key);
                 System.out.println("\n\t\t INPUTED PLAIN TEXT : " + plainText);
-                System.out.println("\t\t GENERATED ENCRYPTION : " +  encryptedText);
+                System.out.println("\t\t GENERATED ENCRYPTION : " + encryptedText);
                 break;
 
             case '2':
@@ -20,7 +22,7 @@ class Playfair{
                 encryptedText = txt.nextLine();
                 plainText = decrypt(encryptedText);
                 System.out.println("\n\t\t INPUTED ENCRYPTED TEXT : " + plainText);
-                System.out.println("\t\t GENERATED DECRYPTION : " +  encryptedText);
+                System.out.println("\t\t GENERATED DECRYPTION : " + encryptedText);
                 break;
 
             case 'X':
@@ -29,17 +31,34 @@ class Playfair{
 
             default:
                 System.out.println("KINDLY ENTER A NUMBER (1-2) or (X) to EXIT ");
-        }//switch statement(int)
-        main(new String[]{});
-    }//end of void main(String[])
+        }// switch statement(int)
+        main(new String[] {});
+    }// end of void main(String[])
 
-    private static String encrypt(String plainText){
+    private static String encrypt(String plainText, String key) {
         String result = "";
+        key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        plainText += " ";
+        int rowF, columnF, rowL, columnL;
+        for (int i = 0; i < plainText.length() - 1; i += 2) {
+            String extract = plainText.substring(i, i + 2);
+
+            if (key.indexOf(extract.charAt(0)) == -1 || key.indexOf(extract.charAt(1)) == -1) {
+                result += extract;
+                continue;
+            } // if statement - NOT IN KEY MATRIX
+
+            rowF = key.indexOf(extract.charAt(0)) / 5;
+            rowL = key.indexOf(extract.charAt(1)) / 5;
+            columnF = key.indexOf(extract.charAt(0)) % 5;
+            columnF = key.indexOf(extract.charAt(1)) % 5;
+
+        } // for loop - i
         return result;
     }
 
-    private static String decrypt(String encryptedText){
+    private static String decrypt(String encryptedText) {
         String result = "";
         return result;
     }
-}//end of class
+}// end of class
