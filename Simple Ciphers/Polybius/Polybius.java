a8be2da5cdf56036f69abf0be26feaaa04c64428
Keane Moraes <lordvader3002@gmail.com>
2020-12-18 22:30:31 +0400

    UPDATE 12.4 - POLYBIUS FIX

--- Simple Ciphers/Polybius/Polybius.java
+++ Simple Ciphers/Polybius/Polybius.java
@@ -1,101 +1,92 @@
 package Polybius;
 import java.util.Scanner;
-
-class Playfair {
+public class Polybius {
     public static void main(String[] args) {
         final Scanner txt = new Scanner(System.in), num = new Scanner(System.in);
-        System.out.println("Enter your choice \n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
+        System.out.println("\nEnter your choice \n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
         final char choice = num.next().toUpperCase().charAt(0);
-        String plainText, encryptedText, key;
+        String plainText, encryptedText;
         switch (choice) {
+            // ENCRYPTION
             case '1':
                 System.out.print("\t PLAIN TEXT : ");
-                plainText = txt.nextLine();
-                System.out.print("\t KEY (every character of the key must be unique) : ");
-                key = txt.nextLine().toUpperCase();
-                encryptedText = encrypt(plainText, key);
+                plainText = txt.nextLine().trim().toUpperCase();
+                encryptedText = encrypt(plainText);
                 System.out.println("\n\t\t INPUTED PLAIN TEXT : " + plainText);
                 System.out.println("\t\t GENERATED ENCRYPTION : " + encryptedText);
                 break;
 
+            // DECRYPTION
             case '2':
                 System.out.print("\t ENCRYPTED TEXT : ");
                 encryptedText = txt.nextLine();
                 plainText = decrypt(encryptedText);
-                System.out.println("\n\t\t INPUTED ENCRYPTED TEXT : " + plainText);
-                System.out.println("\t\t GENERATED DECRYPTION : " + encryptedText);
+                System.out.println("\n\t\t INPUTED ENCRYPTED TEXT : " + encryptedText);
+                System.out.println("\t\t SHIFT : ");
+                System.out.println("\t\t GENERATED DECRYPTION : " + plainText);
                 break;
 
+            // EXIT
             case 'X':
                 System.exit(0);
                 break;
 
             default:
                 System.out.println("KINDLY ENTER A NUMBER (1-2) or (X) to EXIT ");
-        }// switch statement(int)
+        }// switch statement
         main(new String[] {});
     }// end of void main(String[])
 
     /**
-     * POLYBIUS - ENCRYPT 
-     * This function takes 2 parameters - plain text and the
-     * shift value and outputs the encrpytion of the plain text via a Polybius cipher. 
+     * POLYBIUS - ENCRYPT This function takes 2 parameters - plain text and the
+     * shift value and outputs the encrpytion of the plain text via a Polybius
+     * cipher.
+     * 
      * @param plainText - the user input that is to be encrypted
      * @param key       - the text to encrypt the plain text.
      * @return - the encrypted text
      */
-    private static String encrypt(String plainText, String key) {
+    private static String encrypt(String plainText) {
         String result = "";
-        key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        plainText += " ";
-        int rowF, columnF, rowL, columnL;
-        for (int i = 0; i < plainText.length() - 1; i += 2) {
-            String extract = plainText.substring(i, i + 2);
-
-            if (key.indexOf(extract.charAt(0)) == -1 || key.indexOf(extract.charAt(1)) == -1) {
-                result += extract;
+        for (int i = 0; i < plainText.length(); i++) {
+            int letterNumber;
+            if ((plainText.charAt(i) < 65 || plainText.charAt(i) > 90)
+                    && (plainText.charAt(i) < 97 || plainText.charAt(i) > 122)) {
+                result = result.concat(plainText.charAt(i) + "");
                 continue;
-            } // if statement - NOT IN KEY MATRIX
+            } // if statement - special characters
 
-            rowF = key.indexOf(extract.charAt(0)) / 5;
-            rowL = key.indexOf(extract.charAt(1)) / 5;
-            columnF = key.indexOf(extract.charAt(0)) % 5;
-            columnL = key.indexOf(extract.charAt(1)) % 5;
-
-            int encRowF, encRowL, encColumnF, encColumnL;
-            if (rowF == rowL) {
-                encColumnF = (columnF + 1) % 5;
-                encColumnL = (columnL + 1) % 5;
-                encRowF = encRowL = rowL;
-            } else if (columnF == columnL) {
-                encRowF = (rowF + 1) % 5;
-                encRowL = (rowL + 1) % 5;
-                encColumnF = encColumnL = columnF;
-            }//else if statement
-
+            if (plainText.charAt(i) >= 'J')
+                letterNumber = plainText.charAt(i) - 66;
+            else
+                letterNumber = plainText.charAt(i) - 65;
+            int rowNumber = letterNumber / 5 + 1;
+            int columnNumber = letterNumber % 5 + 1;
+            result += (rowNumber + "") + (columnNumber + "") + " ";
         } // for loop - i
         return result;
-    }//end of String encrypt(String, String)
+    }// end of String encrypt(String)
 
     /**
-     * POLYBIUS - DECRYPT
-     * This function takes 2 parameters - encrypted text and the
-     * shift value and outputs the decryption of the encrypted text    
+     * POLYBIUS - DECRYPT This function takes 2 parameters - encrypted text and the
+     * shift value and outputs the decryption of the encrypted text
+     * 
      * @param encryptedText - the user input that is to be encrypted
-     * @param key       - the text to encrypt the plain text.
+     * @param key           - the text to encrypt the plain text.
      * @return - the decrypted text
      */
-    private static String decrypt(String encryptedText){
+    private static String decrypt(String encryptedText) {
         String result = "";
-        encryptedText = encryptedText.trim()+ " ";
-        for(int i = 0; i<encryptedText.length(); i+=3){
-            int letterNumber =  Integer.parseInt(encryptedText.substring(i,i+2));
+        encryptedText = encryptedText.trim() + " ";
+        for (int i = 0; i < encryptedText.length(); i += 3) {
+            int letterNumber = Integer.parseInt(encryptedText.substring(i, i + 2));
             char letter;
-            if(letterNumber/10>2 || (letterNumber/10==2 && letterNumber%10==5))
-                letter = (char)(65 + (letterNumber/10-1)*5 + (letterNumber%10));
-            else letter = (char)(65 + (letterNumber/10-1)*5 + (letterNumber%10-1));
-            result+=letter;
-        }//for loop - i
+            if (letterNumber / 10 > 2 || (letterNumber / 10 == 2 && letterNumber % 10 == 5))
+                letter = (char) (65 + (letterNumber / 10 - 1) * 5 + (letterNumber % 10));
+            else
+                letter = (char) (65 + (letterNumber / 10 - 1) * 5 + (letterNumber % 10 - 1));
+            result += letter;
+        } // for loop - i
         return result;
-    }//end of String decrypt(String)
-}//end of class
+    }// end of String decrypt(String)
+}// end of class
