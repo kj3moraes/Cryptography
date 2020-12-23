59dd980439415e4705a6cc2f435ab3b3631b1aab
Keane Moraes <lordvader3002@gmail.com>
2020-12-23 20:57:53 +0400

    UPDATE 12.8 - Playfair Working Draft

--- Simple Ciphers/Playfair Cipher/Playfair.java
+++ Simple Ciphers/Playfair Cipher/Playfair.java
@@ -1,20 +1,20 @@
 package Polybius;
 import java.util.Scanner;
-class Playfair {
+class Playfair{
     public static void main(String[] args) {
         final Scanner txt = new Scanner(System.in), num = new Scanner(System.in);
         System.out.println("Enter your choice \n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
         final char choice = num.next().toUpperCase().charAt(0);
-        String plainText, encryptedText, key;
+        String plainText, encryptedText,key;
         switch (choice) {
             case '1':
                 System.out.print("\t PLAIN TEXT : ");
-                plainText = txt.nextLine();
+                plainText = txt.nextLine().toUpperCase().trim();
                 System.out.print("\t KEY (every character of the key must be unique) : ");
                 key = txt.nextLine().toUpperCase();
                 encryptedText = encrypt(plainText, key);
                 System.out.println("\n\t\t INPUTED PLAIN TEXT : " + plainText);
-                System.out.println("\t\t GENERATED ENCRYPTION : " + encryptedText);
+                System.out.println("\t\t GENERATED ENCRYPTION : " +  encryptedText);
                 break;
 
             case '2':
@@ -22,7 +22,7 @@ class Playfair {
                 encryptedText = txt.nextLine();
                 plainText = decrypt(encryptedText);
                 System.out.println("\n\t\t INPUTED ENCRYPTED TEXT : " + plainText);
-                System.out.println("\t\t GENERATED DECRYPTION : " + encryptedText);
+                System.out.println("\t\t GENERATED DECRYPTION : " +  encryptedText);
                 break;
 
             case 'X':
@@ -31,34 +31,54 @@ class Playfair {
 
             default:
                 System.out.println("KINDLY ENTER A NUMBER (1-2) or (X) to EXIT ");
-        }// switch statement(int)
-        main(new String[] {});
-    }// end of void main(String[])
+        }//switch statement(int)
+        main(new String[]{});
+    }//end of void main(String[])
 
     private static String encrypt(String plainText, String key) {
         String result = "";
-        key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         plainText += " ";
         int rowF, columnF, rowL, columnL;
-        for (int i = 0; i < plainText.length() - 1; i += 2) {
-            String extract = plainText.substring(i, i + 2);
+        for (int i = 0; i < plainText.length()-1; i+=2) {
+            String extract = plainText.substring(i,i+2);
 
-            if (key.indexOf(extract.charAt(0)) == -1 || key.indexOf(extract.charAt(1)) == -1) {
+            // STEP 1 : WEED OUT THE 'EXTRAS'
+            if(key.indexOf(extract.charAt(0)) == -1 || key.indexOf(extract.charAt(1)) == -1) {
                 result += extract;
                 continue;
-            } // if statement - NOT IN KEY MATRIX
+            }// if statement - NOT IN KEY MATRIX
 
+            // STEP 2 : EXTRACT THE ROWS AND COLUMNS OF BOTH THE CHARACTERS
             rowF = key.indexOf(extract.charAt(0)) / 5;
             rowL = key.indexOf(extract.charAt(1)) / 5;
             columnF = key.indexOf(extract.charAt(0)) % 5;
-            columnF = key.indexOf(extract.charAt(1)) % 5;
+            columnL = key.indexOf(extract.charAt(1)) % 5;
 
-        } // for loop - i
+            // STEP 3 : ENCRYPT BASED ON THE POSITIONS IN THE SQUARE
+            int encRowF = 0, encRowL = 0, encColumnF = 0, encColumnL = 0;
+            if (rowF==rowL){
+                encColumnF = (columnF+1)%5;
+                encColumnL = (columnL+1)%5;
+                encRowF = encRowL = rowL;
+            }// if statement - same ROW
+            else if (columnF==columnL){
+                encRowF = (rowF + 1)%5;
+                encRowL = (rowL + 1)%5;
+                encColumnF = encColumnL = columnF;
+            }// if statement - same COLUMN
+            else {
+                encRowF = rowF;
+                encColumnF = columnL;
+                encRowL = rowL;
+                encColumnL = columnF;
+            }// else - REST OF THE CASES
+            result +=  key.charAt(encRowF*5 + encColumnF) + "" +  key.charAt(encRowL*5 + encColumnL);
+        }// for loop - i
         return result;
-    }
+    }//end of String encrypt(String, String)
 
-    private static String decrypt(String encryptedText) {
+    private static String decrypt(String encryptedText, String key){
         String result = "";
         return result;
-    }
-}// end of class
+    }//end of String decrypt(String, String)
+}//end of class
