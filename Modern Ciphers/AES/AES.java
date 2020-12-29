706347bfc97c9144fc8e2a85fa906a4c2928f3de
Keane Moraes <lordvader3002@gmail.com>
2020-12-29 11:58:35 +0400

    UPDATE 12.13 - AES changes

--- Modern Ciphers/AES/AES.java
+++ Modern Ciphers/AES/AES.java
@@ -9,68 +9,62 @@ public class AES {
     private static String salt;
 
     public static void main(String[] args) {
-        boolean exit = false;
-        do{
-            num = new Scanner(System.in);
-            txt = new Scanner(System.in);
-            salt = "";
-            System.out.println("\nEnter your choice " + "\n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
-            char choice = num.next().toUpperCase().charAt(0);
-            String plainText, encryptedText, seed;
-        
-            switch (choice) {
-                // ENCRYPTION
-                case '1':
-                    System.out.print("\t PLAIN TEXT : ");
-                    plainText = txt.nextLine().trim();
-                    System.out.print("\t SEED (for secret key) : ");
-                    seed = txt.nextLine().toUpperCase().trim();
-                    System.out.print("\t SALT (16 character long string | leave blank if you want a pseudorandom one generated) : ");
-                    salt = txt.nextLine().toUpperCase().trim();                    
-                    encryptedText = encrypt(plainText, seed);
-                    System.out.println("\n\t\t INPUTED PLAIN TEXT : " + plainText);
-                    System.out.println("\t\t KEY (seed for secret key): " + seed);
-                    System.out.println("\t\t SALT (salt for key generation): " + salt);
-                    System.out.println("\t\t GENERATED ENCRYPTION : " + encryptedText);
-                    break;
+        System.out.println("\nEnter your choice " + "\n\t [1] Encrypt \n\t [2] Decrypt \n\t [X] Exit");
+        char choice = num.next().toUpperCase().charAt(0);
+        String plainText, encryptedText, seed;
+        switch (choice) {
+            // ENCRYPTION
+            case '1':
+                System.out.print("\t PLAIN TEXT : ");
+                plainText = txt.nextLine().trim();
+                System.out.print("\t SEED (for secret key) : ");
+                seed = txt.nextLine().toUpperCase().trim();
+                System.out.print(
+                        "\t SALT (16 character long string | leave blank if you want a pseudorandom one generated) : ");
+                salt = txt.nextLine().toUpperCase().trim();
+                encryptedText = encrypt(plainText, seed);
+                System.out.println("\n\t\t INPUTED PLAIN TEXT : " + plainText);
+                System.out.println("\t\t KEY (seed for secret key): " + seed);
+                System.out.println("\t\t SALT (salt for key generation): " + salt);
+                System.out.println("\t\t GENERATED ENCRYPTION : " + encryptedText);
+                break;
 
-                // DECRYPTION
-                case '2':
-                    System.out.print("\t ENCRYPTED TEXT : ");
-                    encryptedText = txt.nextLine().trim();
-                    System.out.print("\t KEY : ");
-                    seed = txt.nextLine().toUpperCase().trim();
-                    System.out.print("\t SALT (the same 16 character string that was used for encryption) : ");
-                    salt = txt.nextLine().toUpperCase().trim();                    
-                    plainText = decrypt(encryptedText, seed);
-                    System.out.println("\n\t\t INPUTED ENCRYPTED TEXT : " + encryptedText);
-                    System.out.println("\t\t KEY (seed for secret key): " + seed);
-                    System.out.println("\t\t GENERATED DECRYPTION : " + plainText);
-                    break;
+            // DECRYPTION
+            case '2':
+                System.out.print("\t ENCRYPTED TEXT : ");
+                encryptedText = txt.nextLine().trim();
+                System.out.print("\t KEY : ");
+                seed = txt.nextLine().toUpperCase().trim();
+                System.out.print("\t SALT (the same 16 character string that was used for encryption) : ");
+                salt = txt.nextLine().toUpperCase().trim();
+                plainText = decrypt(encryptedText, seed);
+                System.out.println("\n\t\t INPUTED ENCRYPTED TEXT : " + encryptedText);
+                System.out.println("\t\t KEY (seed for secret key): " + seed);
+                System.out.println("\t\t GENERATED DECRYPTION : " + plainText);
+                break;
 
-                // EXIT
-                case 'X':
-                    exit = true;
-                    System.exit(0);
-                    break;
+            // EXIT
+            case 'X':
+                System.exit(0);
+                break;
 
-                default:
-                    System.out.println("KINDLY ENTER A NUMBER (1-2) or (X)");
-            }// switch statement
-        }while(!exit);
+            default:
+                System.out.println("KINDLY ENTER A NUMBER (1-2) or (X)");
+        }// switch statement
+        main(new String[] {});
     }// end of void main(String[])
 
     /**
      * AES - ENCRYPT 
-     * This function takes 2 parameters - plain text and the
-     * seed and outputs the encryption of the plain text via the Rjindael Encryption 
+     * This function takes 2 parameters - plain text and the seed and
+     * outputs the encryption of the plain text via the Rjindael Encryption
      * algorithm.
-     * 
+     *
      * @param plainText - the user input that is to be encrypted
      * @param seed      - the text used to generate a key to encrypt the plain text.
      * @return - the encrypted text
      */
-    public static String encrypt(String plaintext, String seed) {
+    public static String encrypt(String plainText, String seed) {
         String result = "";
         System.out.println("\t SUBMIT YOUR AES VERSION: \n\t\t [1] AES-128 \n\t\t [2] AES-192 \n\t\t [3] AES-256");
         char choice = txt.next().trim().charAt(0);
@@ -103,8 +97,8 @@ public class AES {
         int[][] matrix = new int[4][4];
 
         // STEP 2 : PUT THE NECESSARY DATA INTO OUR VARIABLES
-        for (int index = 0; index < plaintext.length(); index++)
-            matrix[index % 4][Math.floorDiv(index, 4)] = plaintext.charAt(index);
+        for (int index = 0; index < plainText.length(); index++)
+            matrix[index % 4][Math.floorDiv(index, 4)] = plainText.charAt(index);
 
         // STEP 3 : USE THE DATA AND THE VARIABLES TO PERFORM THE ENCRYPTION
         // STEP 3.1 - PERFORM THE KEY0 XOR
@@ -124,21 +118,26 @@ public class AES {
         AESRoundFunction.addRoundKey(E.generateKeyMatrix(noOfRounds), matrix);
 
         // STEP 4 : PUT THE RESULTANT MATRIX INTO A HEX STRING
-        for (int c = 0; c < 4; c++)
+        for (int c = 0; c < 4; c++) {
             for (int r = 0; r < 4; r++) {
-                result += Integer.toHexString(matrix[r][c]).length() < 2 ? "0" + Integer.toHexString(matrix[r][c]) : Integer.toHexString(matrix[r][c]);
-            }
+                if (Integer.toHexString(matrix[r][c]).length() < 2)
+                    result += "0" + Integer.toHexString(matrix[r][c]);
+                else
+                    result += Integer.toHexString(matrix[r][c]);
+            } // for loop - rows
+        } // for loop - columns
 
         return result;
     }// end of String encrypt(String, String)
 
     /**
      * AES - DECRYPT 
-     * This function takes 2 parameters - encrypted text and the seed and
-     * outputs the plain text via the Rjindael Decryption  algorithm.
-     * 
+     * This function takes 2 parameters - encrypted text and the seed
+     * and outputs the plain text via the Rjindael Decryption algorithm.
+     *
      * @param encryptedText - the user input that is to be encrypted
-     * @param seed          - the text used to generate the key to decrypt the encrypted text.
+     * @param seed          - the text used to generate the key to decrypt the
+     *                      encrypted text.
      * @return - the decrypted text
      */
     public static String decrypt(String encryptedText, String seed) {
@@ -174,13 +173,14 @@ public class AES {
             matrix[(i / 2) % 4][Math.floorDiv((i / 2), 4)] = Integer.parseInt(encryptedText.substring(i, i + 2), 16);
 
         // STEP 3 : USE THE DATA AND THE VARIABLES TO PERFORM THE ENCRYPTION
-            // STEP 3.1 - PERFORM THE LAST KEY XOR (KEY 10, 12 0R 14 DEPENDING ON AES VERSION)
+            // STEP 3.1 - PERFORM THE LAST KEY XOR (KEY 10, 12 0R 14 DEPENDING ON AES
+            // VERSION)
             AESRoundFunction.addRoundKey(D.getKeyMatrix(noOfRounds), matrix);
             AESRoundFunction.invShiftRows(matrix);
             AESRoundFunction.invSubBytes(matrix);
 
             // STEP 3.2 : ITERATE BACKWARD FROM (noOfRounds-1) TO 1
-            for (int round = noOfRounds-1 ; round > 0 ; round--) {
+            for (int round = noOfRounds - 1; round > 0; round--) {
                 AESRoundFunction.addRoundKey(D.getKeyMatrix(round), matrix);
                 AESRoundFunction.invMixColumns(matrix);
                 AESRoundFunction.invShiftRows(matrix);
@@ -192,15 +192,16 @@ public class AES {
 
         // STEP 4 : PUT THE RESULTANT MATRIX INTO A HEX STRING
         String hex = "";
-        for (int c = 0 ; c < 4 ; c++)
-            for (int r = 0 ; r < 4 ; r++)
-                hex += Integer.toHexString(matrix[r][c]).length() < 2 ? "0" + Integer.toHexString(matrix[r][c]) : Integer.toHexString(matrix[r][c]);
-        
+        for (int c = 0; c < 4; c++)
+            for (int r = 0; r < 4; r++)
+                hex += Integer.toHexString(matrix[r][c]).length() < 2 ? "0" + Integer.toHexString(matrix[r][c])
+                        : Integer.toHexString(matrix[r][c]);
+
         // STEP 5 : CONVERT HEX VALUE TO STRING
         hex += " ";
-        for(int i = 0 ; i < hex.length()-1 ; i+=2)
-            result += (char)(Integer.parseInt(hex.substring(i, i+2), 16)); 
-            
+        for (int i = 0; i < hex.length() - 1; i += 2)
+            result += (char) (Integer.parseInt(hex.substring(i, i + 2), 16));
+
         return result;
     }// end of String decrypt(String, String)
 }// end of class
--- Modern Ciphers/AES/Keys.java
+++ Modern Ciphers/AES/Keys.java
@@ -18,16 +18,17 @@ public class Keys {
 
     public String getInitialKeyState() {
         return initialKeyState;
-    }// end of String getInitKeyState()  
+    }// end of String getInitKeyState()
 
     /**
-     * KEYS - (CONSTRUCTOR)
-     * This constructor takes 3 parameters - a seed for generating the key,
-     * a salt for improving randomness and the algorithm variant which specifies 
-     * the length of the key to be generated. Spits out the pseudorandom key.
-     * 
-     * @param seed - String to generate the key
-     * @param salt - String to improve the randomness of the key
+     * KEYS - (CONSTRUCTOR) 
+     * This constructor takes 3 parameters - a seed for
+     * generating the key, a salt for improving randomness and the algorithm variant
+     * which specifies the length of the key to be generated. Spits out the
+     * pseudorandom key.
+     *
+     * @param seed      - String to generate the key
+     * @param salt      - String to improve the randomness of the key
      * @param algorithm - specifies the length of the key (128, 192 or 256)
      * @return - the 4x4 resultant matrix.
      */
@@ -68,11 +69,11 @@ public class Keys {
 
     /**
      * KEYS - GENERATE KEY MATRIX
-     * This function takes a single parameter - the round number.
-     * It uses the global variable - currentKeyMatrix in conjunction with
-     * the round number to generate the corresponding key for the specified
-     * round based on the key generation procdures specified in Rjindael. 
-     * 
+     * This function takes a single parameter - the round
+     * number. It uses the global variable - currentKeyMatrix in conjunction with
+     * the round number to generate the corresponding key for the specified round
+     * based on the key generation procdures specified in Rjindael.
+     *
      * @param roundNo - specifies the round no.
      * @return - the 4x4 resultant matrix.
      */
@@ -80,8 +81,9 @@ public class Keys {
         int[][] outputMatrix = new int[4][4];
         // STEP 1 : TAKE CARE OF THE KEY0 STATE FOR ALL POSSIBLE KEY LENGTHS
         if (roundNo == 0) {
-            for (int i = 0 ; i < initialKeyState.length() ; i+=2)
-                currentKeyMatrix[(i/2)%4][Math.floorDiv((i/2),4)] = Integer.parseInt(initialKeyState.substring(i,i+2),16);
+            for (int i = 0; i < initialKeyState.length(); i += 2)
+                currentKeyMatrix[(i / 2) % 4][Math.floorDiv((i / 2), 4)] = Integer
+                        .parseInt(initialKeyState.substring(i, i + 2), 16);
             for (int i = 0; i < 4; i++)
                 System.arraycopy(currentKeyMatrix[i], 0, outputMatrix[i], 0, 4);
             return outputMatrix;
@@ -97,13 +99,13 @@ public class Keys {
                     column[i] = currentKeyMatrix[i][3];
 
                 // STEP 2.1.2 : GENERATE THE FIRST COLUMN OF THE NEXT KEY MATRIX
-                column = functionF(column, roundNo-1);
-                for(int i = 0 ; i < 4 ; i++)
+                column = functionF(column, roundNo - 1);
+                for (int i = 0; i < 4; i++)
                     currentKeyMatrix[i][0] ^= column[i];
 
                 // STEP 2.1.3 : USE THE FIRST COLUMN TO GENERATE THE NEXT COLUMNS
                 for (int c = 1; c < 4; c++)
-                    for(int r = 0 ; r < 4 ; r++)
+                    for (int r = 0; r < 4; r++)
                         currentKeyMatrix[r][c] ^= currentKeyMatrix[r][c - 1];
                 break;
 
@@ -116,13 +118,13 @@ public class Keys {
                             column[i] = currentKeyMatrix[i][5];
 
                         // STEP 2.2.0.2 : GENERATE THE FIRST COLUMN OF THE NEXT 4X6 KEY MATRIX
-                        column = functionF(column, roundNo - Math.floorDiv(roundNo,3) - 1);
-                        for(int r = 0 ; r < 4 ; r++)
+                        column = functionF(column, roundNo - Math.floorDiv(roundNo, 3) - 1);
+                        for (int r = 0; r < 4; r++)
                             currentKeyMatrix[r][0] ^= column[r];
 
                         // STEP 2.2.0.3 : USE THE 0th COLUMN TO GENERATE COLUMNS THE NEXT 3 COLUMNS
-                        for (int c = 1 ; c < 4 ; c++)
-                            for(int r = 0 ; r < 4 ; r++)
+                        for (int c = 1; c < 4; c++)
+                            for (int r = 0; r < 4; r++)
                                 currentKeyMatrix[r][c] ^= currentKeyMatrix[r][c - 1];
                         break;
 
@@ -130,33 +132,33 @@ public class Keys {
                         // STEP 2.2.1.1 : GENERATE THE COLUMNS 4 & 5
                         if (roundNo != 1) {
                             for (int c = 4; c < 6; c++)
-                                for(int r = 0 ; r < 4 ; r++)
+                                for (int r = 0; r < 4; r++)
                                     currentKeyMatrix[r][c] ^= currentKeyMatrix[r][c - 1];
-                        }//if statement - ROUND 1 EXCEPTION
+                        } // if statement - ROUND 1 EXCEPTION
 
                         // STEP 2.2.1.2 : ASSIGN THE LAST COLUMN TO A TEMPORARY VARIABLE.
                         for (int i = 0; i < 4; i++)
                             column[i] = currentKeyMatrix[i][5];
 
                         // STEP 2.2.1.3 : GENERATE THE FIRST COLUMN OF THE NEXT KEY 4X6 MATRIX
-                        column = functionF(column, roundNo - Math.floorDiv(roundNo,3) - 1);
-                        for(int r = 0 ; r < 4 ; r++)
+                        column = functionF(column, roundNo - Math.floorDiv(roundNo, 3) - 1);
+                        for (int r = 0; r < 4; r++)
                             currentKeyMatrix[r][0] ^= column[r];
 
                         // STEP 2.2.1.4 : USE THE 0th COLUMN TO GENERATE COLUMN 1
-                        for(int r = 0 ; r < 4 ; r++)
+                        for (int r = 0; r < 4; r++)
                             currentKeyMatrix[r][1] ^= currentKeyMatrix[r][0];
 
                         // STEP 2.2.1.5 : ISOLATE THE COLUMNS 4,5,0,1 INTO A 4x4 PART TO RETURN
                         for (int i = 4; i <= 7; i++)
                             for (int j = 0; j < 4; j++)
-                                outputMatrix[j][i-4] = currentKeyMatrix[j][i % 6];
+                                outputMatrix[j][i - 4] = currentKeyMatrix[j][i % 6];
                         return outputMatrix;
 
                     case 2:
                         // STEP 2.2.2.1 : GENERATE COLUMNS 2,3,4,5
                         for (int c = 2; c <= 5; c++)
-                            for(int r = 0 ; r < 4 ; r++)
+                            for (int r = 0; r < 4; r++)
                                 currentKeyMatrix[r][c] ^= currentKeyMatrix[r][c - 1];
 
                         // STEP 2.2.2.2 : ISOLATE COLUMNS 2,3,4,5 INTO A 4x4 PART
@@ -176,14 +178,14 @@ public class Keys {
                             column[i] = currentKeyMatrix[i][7];
 
                         // STEP 2.3.0.2 : GENERATE THE FIRST COLUMN OF THE NEXT KEY MATRIX
-                        column = functionF(column, (roundNo/2) - 1);
+                        column = functionF(column, (roundNo / 2) - 1);
 
-                        for(int r = 0 ; r < 4 ; r++)
+                        for (int r = 0; r < 4; r++)
                             currentKeyMatrix[r][0] ^= column[r];
 
                         // STEP 2.3.0.3 : USE THE FIRST COLUMN TO GENERATE THE NEXT COLUMNS
-                        for (int c = 1 ; c < 4 ; c++)
-                            for(int r = 0 ; r < 4 ; r++)
+                        for (int c = 1; c < 4; c++)
+                            for (int r = 0; r < 4; r++)
                                 currentKeyMatrix[r][c] ^= currentKeyMatrix[r][c - 1];
                         break;
 
@@ -194,14 +196,14 @@ public class Keys {
 
                         // STEP 2.3.1.2 : GENERATE COLUMN 4 USING COLUMN 3.
                         if (roundNo != 1) {
-                            for(int r = 0 ; r < 4 ; r++)
+                            for (int r = 0; r < 4; r++)
                                 currentKeyMatrix[r][4] ^= S.performSubstitution(currentKeyMatrix[r][3], 'e');
 
                             // STEP 2.3.1.3 : USE THE COLUMN 4 TO GENERATE COLUMNS 5,6,7
                             for (int c = 5; c <= 7; c++)
-                                for(int r = 0 ; r < 4 ; r++)
+                                for (int r = 0; r < 4; r++)
                                     currentKeyMatrix[r][c] ^= currentKeyMatrix[r][c - 1];
-                        }//if statement - ROUND 1 EXCEPTION
+                        } // if statement - ROUND 1 EXCEPTION
 
                         // STEP 2.3.1.4 : ISOLATE THE COLUMNS 4,5,0,1 INTO A 4x4 PART TO RETURN
                         for (int i = 4; i <= 7; i++)
@@ -219,42 +221,45 @@ public class Keys {
     }// end of int[][] generateKeyMatrix(int)
 
     /**
-     * KEYS - GENERATE INVERSE KEY MATRIX
-     * This function takes a single parameter - the total number of rounds. It
-     * constructs a 3D matrix of dimensions (rounds + 1) x 4 x 4 where the ith 2D matrix
-     * corresponds to the (i+1)th round of the key generation protocol. Stores this
-     * in the keyMatrix global variable. Returns nothing.
-     * 
+     * KEYS - GENERATE INVERSE KEY MATRIX 
+     * This function takes a single parameter -
+     * the total number of rounds. It constructs a 3D matrix of dimensions (rounds +
+     * 1) x 4 x 4 where the ith 2D matrix corresponds to the (i+1)th round of the
+     * key generation protocol. Stores this in the keyMatrix global variable.
+     * Returns nothing.
+     *
      * @param noOfRounds - the total number of rounds.
      */
-    protected void generateInverseKeyMatrices(int noOfRounds){    
+    protected void generateInverseKeyMatrices(int noOfRounds) {
         // Step 1 : INITIALIZE KEYMATRIX ARRAY
-        keyMatrix = new int[noOfRounds+1][4][4];
+        keyMatrix = new int[noOfRounds + 1][4][4];
 
         // STEP 2 : GENERATE KEY MATRICES AND STORE IN KEYMATRIX[][][]
-        for (int round = 0 ; round <= noOfRounds ; round++)//generating the key for the round (roundNo-1)
-                keyMatrix[round] = generateKeyMatrix(round);
+        for (int round = 0; round <= noOfRounds; round++)// generating the key for the round (roundNo-1)
+            keyMatrix[round] = generateKeyMatrix(round);
     }// end of void generateInverseKeyMatrices(int)
 
     /**
-     * KEYS - GET KEY MATRIX
-     * This function takes a single parameter - the round number and extracts and returns the 
-     * the corresponding key from the keyMatrix global variable. 
+     * KEYS - GET KEY MATRIX 
+     * This function takes a single parameter - the round
+     * number and extracts and returns the the corresponding key from the keyMatrix
+     * global variable.
      *
      * @param roundNo - the round number
      * @return - the key matrix for the specified round number.
      */
-    protected int[][] getKeyMatrix(int roundNo){
+    protected int[][] getKeyMatrix(int roundNo) {
         return keyMatrix[roundNo];
     }// end of int[][] getKeyMatrix(int)
 
     /**
-     * KEYS - F FUNCTION
-     * This function takes 2 parameters - a column array and the specific RCON number
-     * It returns the resultant column after passing through the F Function as specified
-     * in the Rjindael specification
+     * KEYS - F FUNCTION 
+     * This function takes 2 parameters - a column array and the
+     * specific RCON number It returns the resultant column after passing through
+     * the F Function as specified in the Rjindael specification
      *
-     * @param roundNo - the round number
+     * @param column - the column to process.
+     * @param rconNo - the round number
      * @return - the column after passing through the F Function
      */
     private int[] functionF(int[] column, int rconNo) {
@@ -267,16 +272,17 @@ public class Keys {
         // STEP 2 : SUBBYTES
         for (int i = 0; i < 4; i++)
             resultantKeyColumn[i] = S.performSubstitution(resultantKeyColumn[i], 'e');
-            
+
         // STEP 3 : RCON
         resultantKeyColumn[0] ^= RCON[rconNo];
         return resultantKeyColumn;
     }// end of int[] functionF(int[], int)
 
     /**
-     * KEYS - BYTE ARRAY TO HEX
-     * This function takes a single parameters - a byte array and outputs 
-     * the hex value of each of the elements of the array concatenated together.
+     * KEYS - BYTE ARRAY TO HEX 
+     * This function takes a single parameters - a byte
+     * array and outputs the hex value of each of the elements of the array
+     * concatenated together.
      *
      * @param input - the byte array
      * @return - the concatenated hex value
